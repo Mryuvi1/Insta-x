6d70e414c0f745f3f0bacbc367dbe6af5694cc2a
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-03 21:52:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,3 +82,10 @@ HTML_TEMPLATE = """
 </body>
 </html>
 """
+# (your routes and HTML template code here...)
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
