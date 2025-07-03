185df3696cae77a2dad4f2c1eda46b6694d30e29
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-03 09:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ HTML_TEMPLATE = '''
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Instagram Messaging Bot</title>
+  <title>FUCK THE HATERS</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body {
