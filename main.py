a6ad0da40746392bb45049872b5109dd385ef5ec
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2025-07-05 09:30:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -146,14 +146,14 @@ def index():
                 "thread": thread
             }
 
-            session['username'] = username  # Save session
+            session['username'] = username  # Save login in session
 
             return f"<h3>✅ Message attack started for <b>{username}</b></h3><h5>🗝️ Your STOP Key: <code>{thread_key}</code></h5><br><a href='/'>Back</a>"
 
         except Exception as e:
             return f"<h3>❌ Error: {e}</h3><br><a href='/'>Back</a>"
 
-    # ------- Handle GET request below --------
+    # ==== GET request section ====
     active_keys_html = ""
     session_username = session.get('username')
 
